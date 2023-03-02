883cf684cc672633852514f620c5dd6475a9f14d
Joalor64 <rhgarcon@gmail.com>
2023-03-02 12:38:22 -0500

    source code

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,17 +1,17 @@
 package;
 
-import flixel.text.FlxText;
 import flixel.FlxState;
+import flixel.text.FlxText;
 
 class PlayState extends FlxState
 {
     override public function create()
     {
-        super.create();
-
         var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
         text.screenCenter();
         add(text);
+
+        super.create();
     }
 
     override public function update(elapsed:Float)
